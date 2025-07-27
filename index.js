38b195727d54e5779f7065e6059db46932f17c90
DCHA\diabl <diabl456@naver.com>
2025-07-27 13:50:37 +0900

    버그수정정#

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const  sharp = require('sharp');
-const { S3Client} = require('@aws-sdk/client-s3');
+const { S3Client ,GetObjectCommand, PutObjectCommand} = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client();
 
 
 exports.handler =async (event, context, callback) => {
-    const Bucket = event.Records[0].s3.Bucket.naem;
+    const Bucket = event.Records[0].s3.Bucket.name;
     const Key = decodeURIComponent(event.Records[0].s3.object.key);
     const filename = Key.split('/').at(-1);
     const ext = Key.split('.').at(-1).toLowerCase();
@@ -13,16 +13,21 @@ exports.handler =async (event, context, callback) => {
     console.log( 'name' , filename,'ext',ext);
      
     try {
-        const s3Object = await getObject({Bucket,key}); 
-        console.log('original', s3Object);
-        await sharp(s3Object.Body).resize(200 ,200,{fit:'inside'})
+        const s3Object = await s3.send (new GetObjectCommand({Bucket,key})); 
+        const buffuer = [];
+        for await(const data of GetObjectCommand.Body){
+            buffuer.push(data);
+        }
+        const imageBuffer = Buffer.concat(buffers);
+        console.log('get',imageBuffer.length);
+        const resizedImage = await sharp(imageBuffer).resize(200, 200, { fit: 'inside' })
             .toFormat(requireFormat)
             .toBuffer();
-        await s3.putObject({
+        await s3.send(new PutObjectCommand({
             Bucket,
             Key: `thumb/${filename}`,
             Body: resizedImage,
-        })
+        }));
         console.log('put',resizedImage.length);
         return callback(null,'');
     } catch (error) {
